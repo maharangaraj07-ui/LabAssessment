6bafdfc25191ac19e88ea5acf30d81d3d8a709a2
Mahalakshmi <maha001@gmail.com>
2026-07-27 05:38:38 -0400

    Files Created

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY app.py .
 EXPOSE 5000
 CMD ["python", "app.py"]
+
+
--- app.py
+++ app.py
@@ -5,3 +5,5 @@ def home():
     return "Hello from L3 DevOps Sample App!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+
